7e528a74c40b56d7bd364858e5c4ea743c02b739
ryoma <komars.dev@gmail.com>
2021-05-31 00:20:23 +0900

    更新

--- src/templates/single-blog.js
+++ src/templates/single-blog.js
@@ -1,13 +1,23 @@
 import * as React from "react"
 import { graphql } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
+import Layout from "../components/layout"
+import * as style from "../styles/singleBlog.module.scss"
 
 const SingleBlog = (props) => {
-    return (
-        <div>
-            <h1>{props.data.markdownRemark.frontmatter.title}</h1>
-            <p>{props.data.markdownRemark.frontmatter.date}</p>
-            <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }} />
-        </div>
+    return(
+        <Layout>
+            <div className={style.hero}>
+                <GatsbyImage image={props.data.markdownRemark.frontmatter.image.childImageSharp.gatsbyImageData} alt="blog-image" />
+            </div>
+            <div className={style.wrapper}>  
+                <div className={style.container}>               
+                    <h1>{props.data.markdownRemark.frontmatter.title}</h1>
+                    <p>{props.data.markdownRemark.frontmatter.date}</p>
+                    <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }} />
+                </div>
+            </div>
+        </Layout>                    
     )
 }
 
@@ -15,13 +25,22 @@ export default SingleBlog
 
 export const query = graphql`
   query SingleBlogQuery ($slug: String!) {
-    markdownRemark(fields: { slug: {eq: $slug }}) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
       frontmatter {
         date
         excerpt
         id
-        image
         title
+        image {
+            childImageSharp {
+              gatsbyImageData(            
+                    placeholder: BLURRED
+                    formats: [AUTO, WEBP, AVIF] 
+                    quality: 90
+                    width: 1000
+                )
+            }
+        }        
       }
       html
     }
