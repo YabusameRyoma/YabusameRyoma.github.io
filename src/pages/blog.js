c1d44b17c24f16300de1a8b6831c5a1aef532140
ryoma <komars.dev@gmail.com>
2021-05-30 10:52:33 +0900

    Gatsbyサイト作成

---
+++ src/pages/blog.js
@@ -0,0 +1,42 @@
+import * as React from "react"
+import { graphql,Link } from "gatsby"
+
+const Blog = (props) => {
+    return (
+        <div>
+            <h1>ブログページ</h1>
+            {props.data.allMarkdownRemark.edges.map((singleBlog, index) =>
+                <div key={index}>
+                    <Link to={singleBlog.node.fields.slug}>
+                        <h2>{singleBlog.node.frontmatter.title}</h2>
+                    </Link>
+                    <p>{singleBlog.node.frontmatter.date}</p>
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default Blog
+
+export const query = graphql`
+  query BlogQuery {
+    allMarkdownRemark(sort: {fields: frontmatter___id, order: DESC}) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            date
+            excerpt
+            id
+            image
+            title
+          }
+        }
+      }
+    }
+  }
+  
+`
---
+++ src/templates/single-blog.js
@@ -0,0 +1,29 @@
+import * as React from "react"
+import { graphql } from "gatsby"
+
+const SingleBlog = (props) => {
+    return (
+        <div>
+            <h1>{props.data.markdownRemark.frontmatter.title}</h1>
+            <p>{props.data.markdownRemark.frontmatter.date}</p>
+            <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }} />
+        </div>
+    )
+}
+
+export default SingleBlog
+
+export const query = graphql`
+  query SingleBlogQuery ($slug: String!) {
+    markdownRemark(fields: { slug: {eq: $slug }}) {
+      frontmatter {
+        date
+        excerpt
+        id
+        image
+        title
+      }
+      html
+    }
+  }  
+`
